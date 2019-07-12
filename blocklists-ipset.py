637bb53fddc5367f0dfc7d6cbca6af0d600fab1b
Noel Kuntze <noel@familie-kuntze.de>
2019-07-12 09:10:19 +0200

    Codestyle fixes

--- blocklists-ipset.py
+++ blocklists-ipset.py
@@ -31,17 +31,24 @@ class Network:
         self.network = int(network.network_address)
         self.mask = int(network.netmask)
 
-class blocklists_ipset:
+class BlocklistsIpset:
 
-    blocklists_fqdn= "lists.blocklist.de"
-    blocklists_file = "/lists/all.txt"
+    def __init__(self):
+        self.blocklists_fqdn= "lists.blocklist.de"
+        self.blocklists_file = "/lists/all.txt"
 
-    blocklist_list = list()
-    ipset_v4 = list()
-    ipset_v6 = list()
+        self.blocklist_list = list()
+        self.ipset_v4 = list()
+        self.ipset_v6 = list()
 
-    temporary_name_template = "blocklists-de-temporary"
-    permanent_name_template = "blocklists-de-permanent"
+        self.temporary_name_template = "blocklists-de-temporary"
+        self.permanent_name_template = "blocklists-de-permanent"
+        self.verbose = False
+
+    @classmethod
+    def write_header(self, temporary_file_handle, header):
+        temporary_file_handle.write(bytearray("{}\n".format(header), 'utf-8'))
+        temporary_file_handle.flush()
 
     def get_list(self):
         ctx = ssl.create_default_context()
@@ -49,31 +56,32 @@ class blocklists_ipset:
         ctx.options |= ssl.OP_NO_TLSv1_1
         ctx.options |= ssl.OP_NO_COMPRESSION
         # initiate a secure HTTPS connection to get the list
-        connection = http.client.HTTPSConnection(self.blocklists_fqdn, 
-            context = ctx , timeout=5)
+        connection = http.client.HTTPSConnection(
+            self.blocklists_fqdn,
+            context=ctx, timeout=5)
         try:
             connection.connect()
         except:
-            eprint ("Error while connecting.")
+            eprint("Error while connecting.")
 
         try:
             connection.request("GET", self.blocklists_file)
         except socket.error as e:
-            eprint ("Socket error: {}".format(e))
+            eprint("Socket error: {}".format(e))
             return False
         except socket.timeout as timeout:
-            eprint ("Socket error: Connection timed out.")
+            eprint("Socket error: Connection timed out.")
             return False
 
         response = connection.getresponse()
 
         if response.status != 200:
-            eprint ("Server responded with statuscode {}. Aborting".format(response.statuscode))
+            eprint("Server responded with statuscode {}. Aborting".format(response.statuscode))
             return False
-        
+
         body = response.read()
-        if len(body) == 0:
-            eprint ("Server didn't send us any data.")
+        if body:
+            eprint("Server didn't send us any data.")
             return False
 
         self.blocklist_list = body.decode().split("\n")
@@ -83,18 +91,19 @@ class blocklists_ipset:
 
         number_of_ips = 0
         # set up IPv4 and IPv6 temporary files
-        invalid_v4 = list() 
+        invalid_v4 = list()
         for i in [
-            "0.0.0.0/8", "10.0.0.0/8", "100.64.0.0/10", "172.16.0.0/12",
-            "192.168.0.0/16", "169.254.0.0/16", "255.0.0.0/8",
-            "224.0.0.0/4"
+                "0.0.0.0/8", "10.0.0.0/8", "100.64.0.0/10", "172.16.0.0/12",
+                "192.168.0.0/16", "169.254.0.0/16", "255.0.0.0/8",
+                "224.0.0.0/4"
             ]:
             invalid_v4.append(Network(i))
-        
 
         invalid_v6 = list()
         for i in [
-            "ff00::/8", "fe80::/10", "fd00::/8"
+                "ff00::/8",
+                "fe80::/10",
+                "fd00::/8"
             ]:
             invalid_v6.append(Network(i))
 
@@ -104,17 +113,21 @@ class blocklists_ipset:
         add_template = "add {} {}\n"
 
         temporary_name_v4, temporary_name_v6 = self.derive_names(
-            self.temporary_name_template
-            )
+            self.temporary_name_template)
+
         permanent_name_v4, permanent_name_v6 = self.derive_names(
-            self.permanent_name_template
-            ) 
+            self.permanent_name_template)
 
-        self.write_header(temporary_file_v4.file,
-            self.generate_file_header(temporary_name_v4))
+        self.write_header(
+            temporary_file_v4.file,
+            self.generate_file_header(
+                temporary_name_v4))
 
-        self.write_header(temporary_file_v6.file,
-            self.generate_file_header(temporary_name_v6, family="inet6"))
+        self.write_header(
+            temporary_file_v6.file,
+            self.generate_file_header(
+                temporary_name_v6,
+                family="inet6"))
 
         # write the formatted IPsec record into the corresponding file
         for i in self.blocklist_list:
@@ -126,8 +139,7 @@ class blocklists_ipset:
                     if (int(ipaddress.ip_address(i)) & j.mask) == j.network:
                         continue
                 temporary_file_v4.file.write(bytearray(
-                    add_template.format(temporary_name_v4, i), 'utf-8')
-                )
+                    add_template.format(temporary_name_v4, i), 'utf-8'))
                 number_of_ips += 1
             # else it's IPv6
             else:
@@ -136,8 +148,7 @@ class blocklists_ipset:
                     if (int(ipaddress.ip_address(i)) & j.mask) == j.network:
                         continue
                 temporary_file_v6.file.write(bytearray(
-                    add_template.format(temporary_name_v6, i), 'utf-8')
-                )
+                    add_template.format(temporary_name_v6, i), 'utf-8'))
                 number_of_ips += 1
 
         temporary_file_v4.file.flush()
@@ -148,49 +159,49 @@ class blocklists_ipset:
 
         # swap the set
         self.swap_sets(permanent_name_v4, temporary_name_v4)
-        
+
         # destroy the old set
         self.destroy_set(temporary_name_v4)
 
-        # IPv6 
+        # IPv6
         # load the new records into the new set
         self.restore_file(temporary_file_v6.name)
 
         # swap the set
         self.swap_sets(permanent_name_v6, temporary_name_v6)
-        
+
         # destroy the old set
         self.destroy_set(temporary_name_v6)
 
         if self.verbose:
-            print ("Loaded {} IPs into the sets".format(number_of_ips))
+            print("Loaded {} IPs into the sets".format(number_of_ips))
 
     def restore_file(self, filename):
         cmd = "ipset -exist -f {} restore".format(filename).split(" ")
-        
+
         process = subprocess.Popen(cmd)
         process.wait()
-        
+
         if process.returncode != 0:
             print("Restoring the file {} failed with code {}".format(filename, process.returncode))
             return False
-        
+
     def derive_names(self, name):
         v4 = "_v4"
         v6 = "_v6"
         return "{}{}".format(name, v4), "{}{}".format(name, v6)
-    
+
     def destroy_set(self, set_1):
         cmd = "ipset destroy {}".format(set_1).split(" ")
-        
+
         process = subprocess.Popen(cmd)
         process.wait()
         if process.returncode != 0:
             print("Deleting the ipset {} failed with code {}".format(set_1, process.returncode))
             return False
         return True
-    
-    def swap_sets (self, set_1, set_2):
+
+    def swap_sets(self, set_1, set_2):
         cmd = "ipset swap {} {}".format(set_1, set_2).split(" ")
 
         process = subprocess.Popen(cmd)
@@ -199,14 +210,11 @@ class blocklists_ipset:
             eprint("Swapping the ipsets {} and {} failed with code {}".format(set_1, set_2, process.returncode))
             return False
         return True
-    
-    def write_header(self, temporary_file_handle, header):
-        temporary_file_handle.write(bytearray("{}\n".format(header), 'utf-8'))
-        temporary_file_handle.flush()
-    
 
-    def generate_file_header(self, name, settype="hash:ip", comment=True, family="inet", hashsize=1024, maxelem=65535):
-        format_string=None
+    def generate_file_header(self, name, settype="hash:ip", comment=True, family="inet",
+                             hashsize=1024, maxelem=65535):
+
+        format_string = None
         if comment:
             format_string = "create {} {} family {} hashsize {} maxelem {} comment"
         else:
@@ -215,13 +223,15 @@ class blocklists_ipset:
 
     def run(self):
 
-        parser = argparse.ArgumentParser(description="Updates ipsets with all.txt from blocklist.de.")
-        parser.add_argument('-v',
-                '--verbose',
-                action='store_true',
-                help="Enables verbose mode",
-                dest="verbose"
-            )
+        parser = argparse.ArgumentParser(
+            description="Updates ipsets with all.txt from blocklist.de.")
+        parser.add_argument(
+            '-v',
+            '--verbose',
+            action='store_true',
+            help="Enables verbose mode",
+            dest="verbose"
+        )
 
         args = parser.parse_args()
 
@@ -233,5 +243,5 @@ class blocklists_ipset:
         self.process_blocklist()
 
 if __name__ == '__main__':
-    blocklists = blocklists_ipset()
-    blocklists.run()
+    BLOCKLISTS = BlocklistsIpset()
+    BLOCKLISTS.run()
